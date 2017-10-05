c3531d7ab20a295450ba1a78d4e1c6af0620f470
eisin <eisin@noreply.github.com>
2017-10-05 00:37:25 +0000

    Implemented call status check and wait

--- call_forward_switcher_dcm/__init__.py
+++ call_forward_switcher_dcm/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import twilio.rest
+from twilio.base.exceptions import TwilioRestException
 import urllib
 
 def call_forward_switch(twilio_sid, twilio_token, call_from_phone_number, call_to_phone_number,
@@ -99,11 +100,97 @@ def call_forward_switch(twilio_sid, twilio_token, call_from_phone_number, call_t
     call_record_opt = "false"
     if record_entire:
         call_record_opt = "true"
-    call = twilio_client.calls.create(
-        to=call_to_phone_number,
-        from_=call_from_phone_number,
-        url="http://twimlets.com/echo?Twiml=" + urllib.quote(twiml),
-        record=call_record_opt,
-    )
 
-    return call.sid
+    try:
+        call = twilio_client.calls.create(
+            to=call_to_phone_number,
+            from_=call_from_phone_number,
+            url="http://twimlets.com/echo?Twiml=" + urllib.quote(twiml),
+            record=call_record_opt,
+        )
+    except TwilioRestException as e:
+        return { "sid": None, "call": None, "error": e }
+
+    return { "sid": call.sid, "call": call, "error": None}
+
+def outbound_check_call_finished(twilio_sid, twilio_token, call_sid):
+    if not twilio_sid:
+        raise ValueError("twilio_sid is missing")
+    if not twilio_token:
+        raise ValueError("twilio_token is missing")
+    if not call_sid:
+        raise ValueError("call_sid is missing")
+
+    twilio_client = twilio.rest.Client(twilio_sid, twilio_token)
+
+    try:
+        call = twilio_client.calls(call_sid).fetch()
+    except TwilioRestException as e:
+        return { "finished": True, "status": None, "call": None, "error": e }
+
+    if call.status in {"queued", "ringing", "in-progress"}:
+        return { "finished": False, "status": call.status, "call": call, "error": None }
+    else:
+        return { "finished": True, "status": call.status, "call": call, "error": None }
+
+def outbound_check_call_and_recordings_finished(twilio_sid, twilio_token, call_sid):
+    if not twilio_sid:
+        raise ValueError("twilio_sid is missing")
+    if not twilio_token:
+        raise ValueError("twilio_token is missing")
+    if not call_sid:
+        raise ValueError("call_sid is missing")
+
+    twilio_client = twilio.rest.Client(twilio_sid, twilio_token)
+
+    try:
+        call = twilio_client.calls(call_sid).fetch()
+    except TwilioRestException as e:
+        return { "finished": None, "call_status": None, "recording_status": None, "error": e }
+
+    if call.status in {"queued", "ringing", "in-progress"}:
+        return { "finished": False, "call_status": call.status, "recording_status": None, "error": None }
+
+    try:
+        recordings = twilio_client.recordings.list(call_sid=call_sid)
+    except TwilioRestException as e:
+        return { "finished": None, "call_status": None, "recording_status": None, "error": e }
+
+    recording_status = None
+    for recording in recordings:
+        recording_status = recording.status
+        if recording_status != "completed":
+            return { "finished": False, "call_status": call.status, "recording_status": recording_status, "error": None }
+
+    return { "finished": True, "call_status": call.status, "recording_status": recording_status, "error": None }
+
+def outbound_retreive_recordings(twilio_sid, twilio_token, call_sid):
+    if not twilio_sid:
+        raise ValueError("twilio_sid is missing")
+    if not twilio_token:
+        raise ValueError("twilio_token is missing")
+    if not call_sid:
+        raise ValueError("call_sid is missing")
+
+    twilio_client = twilio.rest.Client(twilio_sid, twilio_token)
+
+    try:
+        recordings = twilio_client.recordings.list(call_sid=call_sid)
+    except TwilioRestException as e:
+        return { "recording_number_confirm_sid": None, "recording_transfer_done_sid": None, "recordings": None, "error": e }
+
+    recording_number_confirm_sid = None
+    recording_transfer_done_sid = None
+    for recording in recordings:
+        duration = int(recording.duration)
+        if 10 - 1 <= duration and duration <= 16 + 1:
+            recording_number_confirm_sid = recording.sid
+        if 5 - 1 <= duration and duration <= 5 + 1:
+            recording_transfer_done_sid = recording.sid
+    return {
+        "recording_number_confirm_sid": recording_number_confirm_sid,
+        "recording_transfer_done_sid": recording_transfer_done_sid,
+        "recordings": recordings,
+        "error": None,
+    }
+
--- switch.py
+++ switch.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import time
 import ConfigParser
 import call_forward_switcher_dcm
 
@@ -11,7 +12,8 @@ forward_to_phone_number = sys.argv[1]
 config = ConfigParser.ConfigParser()
 config.read('switch.cfg')
 
-switch_result = call_forward_switcher_dcm.call_forward_switch(
+print("Call forward switching... (it takes a minute)")
+call_result = call_forward_switcher_dcm.call_forward_switch(
     twilio_sid=config.get("config", "twilio_sid"),
     twilio_token=config.get("config", "twilio_token"),
     call_from_phone_number=config.get("config", "call_from_phone_number"),
@@ -22,4 +24,37 @@ switch_result = call_forward_switcher_dcm.call_forward_switch(
     record_entire=config.getboolean("config", "record_entire"),
     record_response=config.getboolean("config", "record_response"))
 
-print(switch_result)
+if call_result["error"]:
+    print(call_result["error"].message)
+    exit(1)
+
+call_sid = call_result["sid"]
+finished = False
+wait_max = 12
+for i in xrange(wait_max):
+    status = call_forward_switcher_dcm.outbound_check_call_and_recordings_finished(
+        twilio_sid=config.get("config", "twilio_sid"),
+        twilio_token=config.get("config", "twilio_token"),
+        call_sid = call_sid)
+    if status["error"]:
+        print(status["error"].message)
+        exit(1)
+    print "({}/{}) Call status: {}, Recordings status: {}".format(i + 1, wait_max, status["call_status"], status["recording_status"])
+    if status["finished"]:
+        finished = True
+        break
+    time.sleep(10)
+
+if not finished:
+    print("timed out")
+    exit(1)
+
+recordings_result = call_forward_switcher_dcm.outbound_retreive_recordings(
+    twilio_sid=config.get("config", "twilio_sid"),
+    twilio_token=config.get("config", "twilio_token"),
+    call_sid = call_sid)
+if recordings_result["error"]:
+    print(recordings_result["error"].message)
+    exit(1)
+
+print("Call finished. rec1:[{}] rec2:[{}]".format(recordings_result["recording_number_confirm_sid"], recordings_result["recording_transfer_done_sid"]))
