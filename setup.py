781fba535f4de93a2d000029d82fbae194b505a0
eisin <eisin@users.noreply.github.com>
2017-10-17 09:57:51 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='call_forward_switcher_dcm',
+    version='0.1',
+    description='Call forward switcher for dcm',
+    author='eisin',
+    author_email='eisin@noreply.github.com',
+    url='https://github.com/eisin/call-forward-switcher-dcm',
+    install_requires=[
+        'twilio',
+    ],
+    license='Apache License Version 2.0',
+    packages=['call_forward_switcher_dcm'],
+)
