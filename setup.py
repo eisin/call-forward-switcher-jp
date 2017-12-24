e8cb4df28ca44fd38d0a5e3d161b2dfe255357b3
eisin <eisin@users.noreply.github.com>
2017-12-24 13:19:07 +0000

    Bump version 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='call_forward_switcher_dcm',
-    version='0.2',
+    version='0.3',
     description='Call forward switcher for dcm',
     author='eisin',
     author_email='eisin@noreply.github.com',
