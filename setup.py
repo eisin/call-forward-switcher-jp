56cf3d167bee81e9bfa1788319bd8dc581750185
eisin <eisin@users.noreply.github.com>
2019-11-18 16:00:38 +0900

    Python 3 support

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 setup(
-    name='call_forward_switcher_dcm',
-    version='0.4',
-    description='Call forward switcher for dcm',
+    name='call_forward_switcher_jp',
+    version='0.5',
+    description='Call forward switcher for JP career(dcm)',
     author='eisin',
     author_email='eisin@noreply.github.com',
-    url='https://github.com/eisin/call-forward-switcher-dcm',
+    url='https://github.com/eisin/call-forward-switcher-jp',
     install_requires=[
         'twilio',
     ],
     license='Apache License Version 2.0',
-    packages=['call_forward_switcher_dcm'],
+    packages=['call_forward_switcher_jp.dcm'],
 )
